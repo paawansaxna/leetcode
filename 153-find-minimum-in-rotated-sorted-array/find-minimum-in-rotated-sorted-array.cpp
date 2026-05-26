b083cf26eddc0d3fe157f13c9ad303393eec88b2
Paawan Saxena <jec.paawansaxena@gmail.com>
2026-05-26 21:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (9.87%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,21 +1,42 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int left = 0;
+//         int right = nums.size()-1;
+//         while(left<right){
+//             int mid = left + (right-left)/2;
+//             if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+//                 left ++;
+//                 right --;
+//             }
+//             else if(nums[mid]<nums[right]){
+//                 right = mid; 
+//             }
+//             else{
+//                 left = mid + 1;
+//             }
+//         }
+//         return nums[left]; //At the end of binary search: left == right
+//     }
+// };
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int left = 0;
         int right = nums.size()-1;
-        while(left<right){
-            int mid = left + (right-left)/2;
-            if(nums[left] == nums[mid] && nums[mid] == nums[right]){
-                left ++;
-                right --;
-            }
-            else if(nums[mid]<nums[right]){
-                right = mid; 
+        int ans = INT_MAX;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(nums[left]<=nums[mid]){
+                ans = min(ans,nums[left]);
+                left = mid+1;
             }
             else{
-                left = mid + 1;
+                ans = min(ans,nums[mid]);
+                right = mid-1;
             }
         }
-        return nums[left]; //At the end of binary search: left == right
+        return ans;
     }
 };
